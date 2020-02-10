7ae4288fa192901c08efee7779dd65bb3a3c2d6e
Juan Pablo <jp@salamanca.gob.mx>
2020-02-10 14:06:54 -0600

    view of cats for department

--- resources/views/includes/panel/menu.blade.php
+++ resources/views/includes/panel/menu.blade.php
@@ -1,57 +1,19 @@
 <!-- Navigation -->
 <h6 class="navbar-heading text-muted">Gestionar Datos </h6>
-
 <ul class="navbar-nav">
   <li class="nav-item">
     <a class="nav-link" href="{{url('nuevo_predio')}}">
       <i class="ni ni-tv-2 text-red"></i>Predios
     </a>
   </li>
-  <!-- <li class="nav-item">
-    <a class="nav-link" href="{{url('nuevo_predio')}}">
-      <i class="ni ni-planet text-blue"></i>Nuevo Predio
-    </a>
-  </li> -->
-  <!-- <li class="nav-item">
-    <a class="nav-link" href="./examples/maps.html">
-      <i class="ni ni-single-02 text-orange"></i> Médicos
-    </a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="./examples/profile.html">
-      <i class="ni ni-satisfied text-info"></i> Pacientes
-    </a>
-  </li> -->
-  <!-- <li class="nav-item">
-    <a class="nav-link" href="{{route('logout')}}" onclick="event.preventDefault(); document.getElementById('formLogout').submit();">
-      <i class="ni ni-key-25 text-info"></i>Cerrar sesíon
-    </a>
-    <form  action="{{route('logout')}}" method="post" style="display: none;" id="formLogout">
-      @csrf
-    </form>
-  </li> -->
 </ul>
 <hr class="my-3">
 <h6 class="navbar-heading text-muted">Catalogos</h6>
 <ul class="navbar-nav">
+@if(auth()->user()->department == 'catastro')
   <li class="nav-item">
     <a class="nav-link" href="{{url('formas_adquisicion')}}">
       <i class="ni ni-atom text-info"></i> Formas de Adquisición
-    </a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="{{url('propietarios')}}">
-      <i class="ni ni-circle-08 text-orange"></i> Propietarios
-    </a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="{{url('descripcion_predios')}}">
-      <i class="ni ni-bullet-list-67 text-green"></i>Descripción Propiedades
-    </a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link" href="{{url('documentos_soporte')}}">
-      <i class="ni ni-paper-diploma text-primary"></i>Documentos Soporte
     </a>
   </li>
   <li class="nav-item">
@@ -59,6 +21,23 @@
       <i class="ni ni-ungroup text-orange"></i>Tipos de Uso
     </a>
   </li>
+  <li class="nav-item">
+    <a class="nav-link" href="{{url('descripcion_predios')}}">
+      <i class="ni ni-bullet-list-67 text-green"></i>Descripción Propiedades
+    </a>
+  </li>
+  @elseif(auth()->user()->department == 'cpatrimonial')
+  <li class="nav-item">
+    <a class="nav-link" href="{{url('propietarios')}}">
+      <i class="ni ni-circle-08 text-orange"></i> Propietarios
+    </a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="{{url('documentos_soporte')}}">
+      <i class="ni ni-paper-diploma text-primary"></i>Documentos Soporte
+    </a>
+  </li>
+  @endif
   <li class="nav-item">
     <a class="nav-link" href="{{route('logout')}}" onclick="event.preventDefault(); document.getElementById('formLogout').submit();">
       <i class="ni ni-key-25 text-info"></i>Cerrar sesíon
