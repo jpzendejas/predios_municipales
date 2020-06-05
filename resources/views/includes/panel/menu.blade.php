b28da55906df25b1277521017e22406b388186be
Juan Pablo <jp@salamanca.gob.mx>
2020-06-05 10:25:21 -0500

    add full menu to system admin

--- resources/views/includes/panel/menu.blade.php
+++ resources/views/includes/panel/menu.blade.php
@@ -10,7 +10,33 @@
 <hr class="my-3">
 <h6 class="navbar-heading text-muted">Catalogos</h6>
 <ul class="navbar-nav">
-@if(auth()->user()->department == 'catastro')
+@if(auth()->user()->department == 'dgti')
+<li class="nav-item">
+  <a class="nav-link" href="{{url('formas_adquisicion')}}">
+    <i class="ni ni-atom text-info"></i> Formas de Adquisición
+  </a>
+</li>
+<li class="nav-item">
+  <a class="nav-link" href="{{url('uso_tipos')}}">
+    <i class="ni ni-ungroup text-orange"></i>Tipos de Uso
+  </a>
+</li>
+<li class="nav-item">
+  <a class="nav-link" href="{{url('descripcion_predios')}}">
+    <i class="ni ni-bullet-list-67 text-green"></i>Descripción Propiedades
+  </a>
+</li>
+<li class="nav-item">
+  <a class="nav-link" href="{{url('propietarios')}}">
+    <i class="ni ni-circle-08 text-orange"></i> Propietarios
+  </a>
+</li>
+<li class="nav-item">
+  <a class="nav-link" href="{{url('documentos_soporte')}}">
+    <i class="ni ni-paper-diploma text-primary"></i>Documentos Soporte
+  </a>
+</li>
+@elseif(auth()->user()->department == 'catastro')
   <li class="nav-item">
     <a class="nav-link" href="{{url('formas_adquisicion')}}">
       <i class="ni ni-atom text-info"></i> Formas de Adquisición
@@ -51,18 +77,13 @@
 <!-- Divider -->
       <hr class="my-3">
       <!-- Heading -->
-      <h6 class="navbar-heading text-muted">Reportes</h6>
+      <h6 class="navbar-heading text-muted">Actividades</h6>
       <!-- Navigation -->
       <ul class="navbar-nav mb-md-3">
-        <!-- <li class="nav-item">
-          <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/getting-started/overview.html">
-            <i class="ni ni-collection text-yellow"></i> Frecuencia de citas
+        <li class="nav-item">
+          <a class="nav-link" href="https://trello.com/b/w8GJuYBH/predios-municipales-salamanca" target="_blank">
+            <i class="ni ni-collection text-yellow"></i> Acitividades
           </a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="https://demos.creative-tim.com/argon-dashboard/docs/foundation/colors.html">
-            <i class="ni ni-spaceship text-red"></i> Médicos más activos
-          </a>
-        </li> -->
-
       </ul>
+      <hr class="my-3">
